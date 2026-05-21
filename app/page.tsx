3104a44c688990577518376690f23d6191740569
tanashaphotos <tania.espino.delacruz@gmail.com>
2026-05-20 20:06:44 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -244,7 +244,7 @@ export default function Home() {
                 />
               </div>
             </div>
-
+            {/* Descripción de Acerca de */}
             <div className="fade-in text-base md:text-lg text-gray-600 text-center md:text-left">
               Soy Tania Espino, fotógrafa apasionada por capturar emociones reales.
               Mi enfoque combina creatividad, luz natural y detalles para contar historias.
